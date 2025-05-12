67bc9837de7c7810767cd911e1dc7d62f17c5a86
typeshaper <161598560+typeshaper@users.noreply.github.com>
2025-05-12 11:46:42 +0300

    Добавил минимальную ширину для листа и зазор между задачами

--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -1,13 +1,14 @@
 import type { MetaResponse, Todo, TodoInfo } from "../types";
 import TodoItem from "./TodoItem";
 import { useRouteLoaderData } from "react-router";
+import classes from "./TodoList.module.css";
 
 export default function TodoList() {
   const data: MetaResponse<Todo, TodoInfo> = useRouteLoaderData("todo-page")!;
   const todoItems: Todo[] = data.data;
 
   return (
-    <ul>
+    <ul className={classes.list}>
       {todoItems.map((todo) => (
         <TodoItem
           key={todo.id}
