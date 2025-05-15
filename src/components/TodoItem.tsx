33f0b7be348e64d3cd3ad82021f545ea20f92d1e
typeshaper <161598560+typeshaper@users.noreply.github.com>
2025-05-15 15:59:37 +0300

    Добавил к компоненту TodoItem кнопку удаления задачи

--- src/components/TodoItem.tsx
+++ src/components/TodoItem.tsx
@@ -2,14 +2,26 @@ import type { Todo } from "../types";
 import classes from "./TodoItem.module.css";
 import isDoneIcon from "../assets/checkbox-done.png";
 import isNotDone from "../assets/checkbox-undone.png";
+import deleteIcon from "../assets/delete.png";
 
 export default function TodoItem({ todo }: { todo: Todo }) {
-  const { title, isDone } = todo;
+  const { title, isDone, id } = todo;
 
   return (
     <li>
-      <img src={isDone ? isDoneIcon : isNotDone} />
+      <img
+        className={classes["task-status-icon"]}
+        src={isDone ? isDoneIcon : isNotDone}
+      />
       <p className={isDone ? classes["text-done"] : ""}>{title}</p>
+      <div className={classes["icons-wrapper"]}>
+        <div className={classes["delete-icon-wrapper"]}>
+          <img
+            className={classes["deleteIcon"]}
+            src={deleteIcon}
+          />
+        </div>
+      </div>
     </li>
   );
 }
