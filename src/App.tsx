c348142f93b2dd9d5c52e11f8b8edc0996a821c9
typeshaper <161598560+typeshaper@users.noreply.github.com>
2025-06-14 12:03:11 +0300

    Add routes for app and auth sections

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,26 @@
 import "./App.css";
-import RootLayoutPage from "./pages/RootLayoutPage";
-import TodoListPage from "./pages/TodoListPage";
-import ProfilePage from "./pages/ProfilePage";
-import { createBrowserRouter, RouterProvider, Navigate } from "react-router";
+import AuthLayout from "./layouts/AuthLayout";
+import AppLayout from "./layouts/AppLayout";
+import TodoListPage from "./pages/app/TodoListPage";
+import ProfilePage from "./pages/app/ProfilePage";
+import { createBrowserRouter, RouterProvider } from "react-router";
 import { App as AntdApp } from "antd";
 
 const router = createBrowserRouter([
   {
-    path: "/",
-    element: <RootLayoutPage />,
+    path: "/app",
+    element: <AppLayout />,
     children: [
-      {
-        index: true,
-        element: (
-          <Navigate
-            to="/tasks"
-            replace
-          />
-        ),
-      },
-      { path: "/tasks", element: <TodoListPage /> },
-      { path: "/profile", element: <ProfilePage /> },
+      { path: "tasks", element: <TodoListPage /> },
+      { path: "profile", element: <ProfilePage /> },
+    ],
+  },
+  {
+    path: "/auth",
+    element: <AuthLayout />,
+    children: [
+      { path: "sign-up", element: <TodoListPage /> },
+      { path: "login", element: <ProfilePage /> },
     ],
   },
 ]);
