fd2a945a433f5428c393fd59320e75767fda3b99
typeshaper <161598560+typeshaper@users.noreply.github.com>
2025-06-16 10:38:34 +0300

    Create api call for login

---
+++ src/api/auth.ts
@@ -0,0 +1,25 @@
+import axios, { AxiosError, type AxiosResponse } from "axios";
+import type { AuthData } from "../types/todo";
+import type { Token } from "../types/auth";
+
+const api = axios.create({
+  baseURL: `https://easydev.club/api/v1`,
+});
+
+export async function login(loginData: AuthData) {
+  try {
+    const response: AxiosResponse = await api({
+      method: "post",
+      url: "/auth/signin",
+      data: loginData,
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+
+    const resData: Token = await response.data;
+    return resData;
+  } catch (error: unknown) {
+    throw error as AxiosError;
+  }
+}
