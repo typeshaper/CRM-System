9a581990e24981399de3b12e1292d344e7c3996f
typeshaper <161598560+typeshaper@users.noreply.github.com>
2025-06-18 16:07:47 +0300

    Remove refresh token from store

--- src/store/auth.ts
+++ src/store/auth.ts
@@ -1,30 +1,15 @@
 import { createSlice } from "@reduxjs/toolkit";
-import type { Token } from "../types/auth";
 
-const initialState: Token = {
+const initialState = {
   accessToken: "",
-  refreshToken: "",
-};
-
-const hasValidToken = (token: Token): token is Token => {
-  return (
-    "accessToken" in token &&
-    "refreshToken" in token &&
-    typeof token.accessToken === "string" &&
-    typeof token.refreshToken === "string"
-  );
 };
 
 const authSlice = createSlice({
   name: "auth",
   initialState,
   reducers: {
-    setTokens(state, action) {
-      if (hasValidToken(action.payload)) {
-        const { accessToken, refreshToken } = action.payload;
-        state.accessToken = accessToken;
-        state.refreshToken = refreshToken;
-      }
+    setAccessToken(state, action) {
+      state.accessToken = action.payload;
     },
   },
 });
