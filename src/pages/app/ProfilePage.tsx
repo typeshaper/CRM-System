eb89c6a45c58d7d1ccc066cbcaaf829d45a9f190
typeshaper <161598560+typeshaper@users.noreply.github.com>
2025-06-20 11:22:46 +0300

    Create layout of personal info list

--- src/pages/app/ProfilePage.tsx
+++ src/pages/app/ProfilePage.tsx
@@ -1,8 +1,25 @@
-import { Typography } from "antd";
+import { Typography, List } from "antd";
 
 const ProfilePage = () => {
-  const { Title } = Typography;
-  return <Title>Hey there!</Title>;
+  const { Title, Text } = Typography;
+  return (
+    <>
+      <Title>Your info</Title>
+      <List bordered>
+        <List.Item>
+          <Text>Username: dimasasas</Text>
+        </List.Item>
+
+        <List.Item>
+          <Text>Email: bobikchmobik@mama.cock</Text>
+        </List.Item>
+
+        <List.Item>
+          <Text>Phone: 8777777777777</Text>
+        </List.Item>
+      </List>
+    </>
+  );
 };
 
 export default ProfilePage;
