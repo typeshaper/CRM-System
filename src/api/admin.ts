4bbe57e3f579ece04316c5425d064f4c4c44281d
typeshaper <161598560+typeshaper@users.noreply.github.com>
2025-07-03 16:35:41 +0300

    Create api call for updating userdata

--- src/api/admin.ts
+++ src/api/admin.ts
@@ -2,6 +2,7 @@ import { AxiosError, type AxiosResponse } from "axios";
 import { api } from "./index";
 import type {
   Profile,
+  ProfileRequest,
   User,
   UserFilters,
   UsersMetaResponse,
@@ -30,6 +31,30 @@ export async function getUserById(id: Profile["id"]): Promise<Profile> {
     const response: AxiosResponse<Profile> = await api({
       method: "get",
       url: `admin/users/${id}`,
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+
+    const resData = response.data;
+    return resData;
+  } catch (error: unknown) {
+    throw error as AxiosError;
+  }
+}
+
+export async function updateUserData(
+  newData: ProfileRequest,
+  id: Profile["id"]
+): Promise<Profile> {
+  try {
+    const response: AxiosResponse<Profile> = await api({
+      method: "post",
+      url: `admin/users/${id}`,
+      data: newData,
+      headers: {
+        "Content-Type": "application/json",
+      },
     });
 
     const resData = response.data;
