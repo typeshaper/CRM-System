9785867b21406286c569ce1171c533abf259ae2d
typeshaper <161598560+typeshaper@users.noreply.github.com>
2025-07-03 15:18:29 +0300

    Create api call for getting user by id

--- src/api/admin.ts
+++ src/api/admin.ts
@@ -1,6 +1,11 @@
 import { AxiosError, type AxiosResponse } from "axios";
 import { api } from "./index";
-import type { User, UserFilters, UsersMetaResponse } from "../types/user";
+import type {
+  Profile,
+  User,
+  UserFilters,
+  UsersMetaResponse,
+} from "../types/user";
 
 export async function getUsersList(queryParams?: UserFilters) {
   try {
@@ -14,6 +19,20 @@ export async function getUsersList(queryParams?: UserFilters) {
     });
 
     const resData: UsersMetaResponse<User> = await response.data;
+    return resData;
+  } catch (error: unknown) {
+    throw error as AxiosError;
+  }
+}
+
+export async function getUserById(id: Profile["id"]): Promise<Profile> {
+  try {
+    const response: AxiosResponse<Profile> = await api({
+      method: "get",
+      url: `admin/users/${id}`,
+    });
+
+    const resData = response.data;
     return resData;
   } catch (error: unknown) {
     throw error as AxiosError;
