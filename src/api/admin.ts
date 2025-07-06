91f74c0eaadac60ea3482bd39937382a6cce08b8
typeshaper <161598560+typeshaper@users.noreply.github.com>
2025-07-06 16:46:19 +0300

    Add api call for changing user roles

--- src/api/admin.ts
+++ src/api/admin.ts
@@ -2,7 +2,12 @@ import { AxiosError, type AxiosResponse } from "axios";
 import { api } from "./index";
 import type { Profile, ProfileRequest } from "../types/user";
 
-import type { User, UserFilters, UsersMetaResponse } from "../types/admin.ts";
+import type {
+  User,
+  UserFilters,
+  UserRolesRequest,
+  UsersMetaResponse,
+} from "../types/admin.ts";
 
 export async function getUsersList(queryParams?: UserFilters) {
   try {
@@ -99,6 +104,32 @@ export async function unblockUser(id: Profile["id"]): Promise<Profile> {
       headers: {
         "Content-Type": "application/json",
       },
+    });
+
+    const resData = response.data;
+    return resData;
+  } catch (error: unknown) {
+    throw error as AxiosError;
+  }
+}
+// 3. Обновление прав пользователя
+// Method: PUT
+// URL: /admin/users/{id}/rights
+// Request: UserRolesRequest
+// Response: User
+
+export async function editRoles(
+  id: Profile["id"],
+  roles: UserRolesRequest
+): Promise<User> {
+  try {
+    const response: AxiosResponse<User> = await api({
+      method: "put",
+      url: `admin/users/${id}/rights`,
+      headers: {
+        "Content-Type": "application/json",
+      },
+      data: roles,
     });
 
     const resData = response.data;
